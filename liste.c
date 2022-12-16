881216e3ed9f12bf95bebbc997155146fd83a2e6
loris <loris.obry@gmail.com>
2022-12-16 16:43:46 +0100

    structure MaillonDept et structure VilleIut

---
+++ liste.c
@@ -0,0 +1,2 @@
+#include "sae.h"
+
--- sae.h
+++ sae.h
@@ -3,3 +3,17 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct maillonDept
+{
+    char nomDept[30];
+    char resp[30];
+    int nbP;
+    struct maillonDept *suiv;
+}MaillonDept, *Ldept;
+
+// Utilise pour le tableau de pointeurs sur cette structure (pour Guillaume)
+typedef struct 
+{
+    char nom[30];
+    Ldept l;
+}VilleIut;
