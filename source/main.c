c39884164926ad0ef77f03ea9f554320fae22f1b
DahmaneYanis <dahmane.yanis.2004@gmail.com>
2023-01-02 15:05:29 +0100

    Réorganisation du projet

---
+++ source/main.c
@@ -0,0 +1,15 @@
+#include "../header/sae.h"
+
+
+int main(void)
+{
+	ListeDept ldept;
+	VilleIut iut;
+
+	//TESTS des fonctions de manipulation des structures VilleIut et ListeDept
+	iut.ldept = listenouv();
+	strcpy(iut.nom, "Clermont Ferrand");
+	iut.ldept = ajouterDept(iut.ldept, "Chimie", "Henri Laserre", 80);
+	iut.ldept = ajouterDept(iut.ldept, "Chimie", "Henri Laserre", 80);
+	afficherListe(iut.ldept);
+}
