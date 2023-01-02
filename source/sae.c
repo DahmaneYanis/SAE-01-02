24c5816bbb51fec9404e5d78147df400d7403349
DahmaneYanis <dahmane.yanis.2004@gmail.com>
2023-01-02 15:16:00 +0100

    Création de la fonction globale, documentée et en attente des fonctions de chargement et de sauvegarde + création du menuVisiteur

--- source/sae.c
+++ source/sae.c
@@ -1,2 +1,22 @@
 #include "../header/sae.h"
 
+/**
+ * @brief Fonction globale qui :
+ * - Charge les fichiers dans les structures adaptées
+ * - Appel la fonction du menu général (visiteur) avec les données enregistrées
+ * - Sauvegarde toutes les données dans les fichiers correspondants avant de terminer le logiciel
+ */
+void Globale(void)
+{
+    //Chargement des fichiers
+
+    //Appel du menu visiteur
+    menuVisiteur();
+
+    //Sauvegarde dans les fichiers
+}
+
+void menuVisiteur(void)
+{
+    return;
+}
