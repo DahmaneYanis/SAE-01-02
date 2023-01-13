5313ded9ebd6ffd9e340e4aa011ea2ae850e2067
loris OBRY <loris.obry@gmail.com>
2023-01-13 23:13:39 +0100

    phase.c -menuAdmin -afficherMenuAdmin

--- source/main.c
+++ source/main.c
@@ -5,29 +5,6 @@
 int main(void)
 {
 
-	// #ifdef _WIN32
-	// color
-	// #endif
-<<<<<<< HEAD
-=======
-	//guillaume();
->>>>>>> b092b29fcec9a95286488850700787d4cc787a2c
-	//testCharge();
 	Globale();
-	//testJean();
 	return 0;
-
-	//system("color 01");
-
-	/*
-	ListeDept ldept;
-	VilleIut iut;
-
-	//TESTS des fonctions de manipulation des structures VilleIut et ListeDept
-	iut.ldept = listenouv();
-	strcpy(iut.nom, "Clermont Ferrand");
-	iut.ldept = ajouterDept(iut.ldept, "Chimie", "Henri Laserre", 80);
-	iut.ldept = ajouterDept(iut.ldept, "Chimie", "Henri Laserre", 80);
-	afficherListe(iut.ldept);
-	*/
 }
