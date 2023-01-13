180d2991db4d352e67187c2ea39ffe0ad0ceec48
loris OBRY <loris.obry@gmail.com>
2023-01-13 17:09:11 +0100

    Changement organisation des fonctions

--- header/sae.h
+++ header/sae.h
@@ -137,10 +137,6 @@ int creerCandidat(Candidat *tCand[], int nbCandidats);
 
 ListeDeptV2 configurationDeptV2( ListeDept ldept );
 
-void affichageListesDept( ListeCandidats lcand, char * type, char * nomDept );
-ListeCandidats trierListeCandidats( ListeCandidats l );
-ListeCandidats insertionCroissanteCand( ListeCandidats nvL, ListeCandidats l );
-ListeCandidats insertionTeteCand( ListeCandidats nvL, ListeCandidats l );
 
 // listeDeparements.c
 ListeDept ajouterEnTete(ListeDept ldept, char nomDept[], char resp[], int nbP);
@@ -181,3 +177,7 @@ void tailleSupTabIut(VilleIut ** tIut, int *nbMax);
 // candidat.c
 
 Candidat *modifierCandidat(Candidat *c);
+void affichageListesDept( ListeCandidats lcand, char * type, char * nomDept );
+ListeCandidats trierListeCandidats( ListeCandidats l );
+ListeCandidats insertionCroissanteCand( ListeCandidats nvL, ListeCandidats l );
+ListeCandidats insertionTeteCand( ListeCandidats nvL, ListeCandidats l );
